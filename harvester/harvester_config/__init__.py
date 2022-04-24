a42ec19395040c28f275e54f5c51332c10d18690
Lulu Ilmaknun Qurotaini <lulu.ilmaknun@ui.ac.id>
2022-04-24 12:57:50 +1000

    Update config package management

---
+++ harvester/harvester_config/__init__.py
@@ -0,0 +1,16 @@
+AUTH = {
+    "CONSUMER_KEY": "mxqyJYQQTjK8GELo7WuyF7PZL",
+    "CONSUMER_KEY_SECRET": "GCoiXIGJhixATDaGJPxnRsDAl8wkRFFSGPCQkZdeproCIrfKCV",
+    "ACCESS_TOKEN": "1514125532824502275-aGDxWMrxR4Qc1FCip9rxOww7vnGrQu",
+    "ACCESS_TOKEN_SECRET": "VY74YrL3o9T48nxM5i4lBKajESQxtX6WYFUlETUYKR2HT",
+}
+
+KEYWORDS = [
+    "drive electric", "electric future", "electric car", "electric vehicle", "modelx", "ev conversion",
+    "elon musk", "tesla ", "tesla life", "tesla car", "tesla roadster", "tesla motors", "tesla model",
+    "car charger", "self driving", "urban mobility", "zero emissions", "electric mobility", "emobility",
+    "self driving car", "autonomous vehicle", "autonomous car", "future car", "ev sales", "ev battery",
+    "autonomous driving", "audietron", "alternative fuel", "connected vehicle", "connected car",
+    "mahindra"]
+
+LOCATIONS = [144.6845, -38.0815, 145.3540, -37.5934]
--- harvester/stream.py
+++ harvester/stream.py
@@ -2,24 +2,30 @@ import tweepy as tw
 import json
 
 from streamer import StreamListener
+from harvester_config import AUTH, KEYWORDS, LOCATIONS
 
 
 # Read configuration
-with open('config.json', 'r') as f:
-    config = json.load(f)
+keywords = []
+for keyword in KEYWORDS:
+    hash = "#"
+    for word in keyword:
+        hash += word
+    keywords.append(hash)
+keywords += KEYWORDS
 
 # Connect to API stream
-auth = tw.OAuthHandler(config["AUTH"]["CONSUMER_KEY"], 
-                           config["AUTH"]["CONSUMER_KEY_SECRET"])
-auth.set_access_token(config["AUTH"]["ACCESS_TOKEN"], 
-                      config["AUTH"]["ACCESS_TOKEN_SECRET"])
+auth = tw.OAuthHandler(AUTH["CONSUMER_KEY"], 
+                       AUTH["CONSUMER_KEY_SECRET"])
+auth.set_access_token(AUTH["ACCESS_TOKEN"], 
+                      AUTH["ACCESS_TOKEN_SECRET"])
 api = tw.API(auth, wait_on_rate_limit=True)
 
 # Stream
-mystream_listener = StreamListener(keywords=config["KEYWORDS"])
+mystream_listener = StreamListener(keywords=KEYWORDS,
 mystream = tw.Stream(auth=api.auth, listener=mystream_listener)
 
 # Filter twitter
 print("Start streaming...")
 mystream.filter(languages = ["en"], 
-                locations=config["LOCATIONS"])
+                locations=LOCATIONS)
