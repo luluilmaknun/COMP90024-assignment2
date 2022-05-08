6cf01669f434a4b87a5cf450260e8b05afae4bd6
rbghks0126 <40514272+rbghks0126@users.noreply.github.com>
2022-05-08 11:53:10 +1000

    update bounding box coordinates

--- harvester/harvester_config/__init__.py
+++ harvester/harvester_config/__init__.py
@@ -13,12 +13,12 @@ AUTH = {
         "ACCESS_TOKEN": "1514125532824502275-aGDxWMrxR4Qc1FCip9rxOww7vnGrQu",
         "ACCESS_TOKEN_SECRET": "VY74YrL3o9T48nxM5i4lBKajESQxtX6WYFUlETUYKR2HT"
     },
-    # gyus
+    # rislas
     'north': {
-        "CONSUMER_KEY": "JhsoL6IczdxoEJR1YO9XGdvmD",
-        "CONSUMER_KEY_SECRET": "mQBSn7CWZsjgOK71pBb2E2ovhQiaksXMDmRTNhoqsXcLZrSJHd",
-        "ACCESS_TOKEN": "1513827450698235907-TOFjLbkVCZpb66kCdJqXLZ6D7jmDVu",
-        "ACCESS_TOKEN_SECRET": "bzTSp8Z1TRbwEGQCYS1vvnS0DKXmZMNJNXVrCDMZzuHJ7"
+        "CONSUMER_KEY": "WpoHCTZpdnBO9GDDb8TaiCPE4",
+        "CONSUMER_KEY_SECRET": "stGYLL8cV9VcUSh2wljrkbw9cXQuMoisFQG9e19eH6QfWoIdHe",
+        "ACCESS_TOKEN": "1515880143889510400-DRbcnUvKAEBm6JRpSm7rtwbK9ykfJx",
+        "ACCESS_TOKEN_SECRET": "UQJk5ZtYk0ncr5Vq8Ko6WCI3pJiobgCts9gWO08sHjc1t"
     },
     # gyus
     'south': {
@@ -42,7 +42,7 @@ KEYWORDS = {
         'biodegradable', 'yellow bin', 'e-waste', 'reusable', 'reprocessing', 'recycled', 'recyclebots',
         'waste paper', 'carbon neutral', 'reduce'],
     'solar': [
-        'solar', 'solar panels', 'solar battery', 'csp', 'renewable energy', 'elon musk', 
+        'solar', 'solar panels', 'solar battery', 'csp', 'renewable energy', 
         'sun power', 'photovoltaic', 'cell', 'wind turbine', 'hydro', 'solar city', 'windmill',
         'green energy', 'geothermal', 'biomass', 'carbon tax', 'carbon policy', 'C02 footprint', 
         'EBITDA', 'Enova', 'Diamond Energy', 'Momentum Energy', 'Aurora Energy', 'Tilt', 'WestWind',
@@ -52,10 +52,10 @@ KEYWORDS = {
 
 # LOCATIONS = [144.6845, -38.0815, 145.3540, -37.5934]
 LOCATIONS = {}
-LOCATIONS["west"] = [144.317, -38.012, 144.856, -37.166]
-LOCATIONS["north"] = [144.856, -38.012, 145.107, -37.166]
-LOCATIONS["east"] = [145.115, -37.997, 145.490, -37.423]
-LOCATIONS["south"] = [144.629, -38.533, 145.546, -38.012]
+LOCATIONS["west"] = [144.317, -38.0, 144.856, -37.166]
+LOCATIONS["north"] = [144.856, -38.0, 145.185, -37.166]
+LOCATIONS["east"] = [145.185, -38.0, 145.901, -37.166]
+LOCATIONS["south"] = [144.655, -38.533, 145.901, -38.0]
 
 
 
