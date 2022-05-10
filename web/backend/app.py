475cf27fc617fc63ba8855670b0209c6b0112d8f
Risla Jabarullah <jabarullahf@student.unimelb.edu.au>
2022-05-10 19:51:27 +1000

    Added 3 endpoints to get the data by keywords

--- web/backend/app.py
+++ web/backend/app.py
@@ -1,3 +1,5 @@
+from ast import Param
+from inspect import Parameter
 import couchdb
 
 from flask_cors import CORS
@@ -23,24 +25,29 @@ class Tweet(Resource):
             db = couch[db_name]
             rows = db.view('_all_docs', include_docs=True)
             data = [row['doc'] for row in rows]
-            output += data        
+            output += data
 
         return jsonify(
             total=len(output),
             rows=output
         )
 
+# Function to return data from north melbourne databases and aurin data
+
+
 class TweetOfNorth(Resource):
     def get(self):
-        # Function to return view from north melbourne from databases
         print("Entering database")
+        # get the related aurin data
         aurin_result = get_aurin(search_by="region", search_region="north")
         test = []
 
-        db_name ='twitter_north'
+        # query couchdb view to get the related data
+        db_name = 'twitter_north'
         if db_name in DB_LIST:
             db = couch[db_name]
-            rows = db.view('SentimentInfo/SentimentOfKey', reduce=True, group=True, group_level=2)
+            rows = db.view('SentimentInfo/SentimentOfKey',
+                           reduce=True, group=True, group_level=2)
             for item in rows:
                 result = {}
                 result["key"] = item.key
@@ -48,18 +55,23 @@ class TweetOfNorth(Resource):
                 test.append(result)
 
         return jsonify(output=test, aurin=aurin_result)
+
+# Function to return data from south melbourne databases and aurin data
+
 
 class TweetOfSouth(Resource):
     def get(self):
-        # Function to return view from north melbourne from databases
         print("Entering database")
+        # get the related aurin data
         aurin_result = get_aurin(search_by="region", search_region="south")
         test = []
 
-        db_name ='twitter_south'
+        # query couchdb view to get the related data
+        db_name = 'twitter_south'
         if db_name in DB_LIST:
             db = couch[db_name]
-            rows = db.view('SentimentInfo/SentimentOfKey', reduce=True, group=True, group_level=2)
+            rows = db.view('SentimentInfo/SentimentOfKey',
+                           reduce=True, group=True, group_level=2)
             for item in rows:
                 result = {}
                 result["key"] = item.key
@@ -67,18 +79,23 @@ class TweetOfSouth(Resource):
                 test.append(result)
 
         return jsonify(output=test, aurin=aurin_result)
+
+# Function to return data from west melbourne databases and aurin data
+
 
 class TweetOfWest(Resource):
     def get(self):
-        # Function to return view from north melbourne from databases
         print("Entering database")
+        # get the related aurin data
         aurin_result = get_aurin(search_by="region", search_region="west")
         test = []
 
-        db_name ='twitter_west'
+        # query couchdb view to get the related data
+        db_name = 'twitter_west'
         if db_name in DB_LIST:
             db = couch[db_name]
-            rows = db.view('SentimentInfo/SentimentOfKey', reduce=True, group=True, group_level=2)
+            rows = db.view('SentimentInfo/SentimentOfKey',
+                           reduce=True, group=True, group_level=2)
             for item in rows:
                 result = {}
                 result["key"] = item.key
@@ -86,18 +103,23 @@ class TweetOfWest(Resource):
                 test.append(result)
 
         return jsonify(output=test, aurin=aurin_result)
+
+# Function to return data from east melbourne databases and aurin data
+
 
 class TweetOfEast(Resource):
     def get(self):
-        # Function to return view from north melbourne from databases
         print("Entering database")
+        # get the related aurin data
         aurin_result = get_aurin(search_by="region", search_region="east")
         test = []
 
-        db_name ='twitter_east'
+        # query couchdb view to get the related data
+        db_name = 'twitter_east'
         if db_name in DB_LIST:
             db = couch[db_name]
-            rows = db.view('SentimentInfo/SentimentOfKey', reduce=True, group=True, group_level=2)
+            rows = db.view('SentimentInfo/SentimentOfKey',
+                           reduce=True, group=True, group_level=2)
             for item in rows:
                 result = {}
                 result["key"] = item.key
@@ -105,13 +127,207 @@ class TweetOfEast(Resource):
                 test.append(result)
 
         return jsonify(output=test, aurin=aurin_result)
+
+# Function to return data on recycling in all regoin of melbourne from couchdb views and aurin data
+
+
+class TweetsOnRecycling(Resource):
+    def get(self):
+        print("Entering database")
+        # get the related aurin data
+        # aurin_result = get_aurin(search_by="topic", search_topic="recycling")
+        test = []
+        regions = []
+        labels = []
+
+        # query couchdb view to get the related data
+        for db_name in DB_LIST:
+            region = ''
+            match db_name:
+                case "twitter_north":
+                    region = "north"
+                case "twitter_south":
+                    region = "south"
+                case "twitter_west":
+                    region = "west"
+                case "twitter_east":
+                    region = "east"
+                case _:
+                    continue
+
+            db = couch[db_name]
+            rows = db.view('SentimentInfo/SentimentOfKey',
+                           reduce=True, group=True, group_level=2)
+
+            for item in rows:
+                curent_key = item.key
+                if(curent_key[0] == 'recycling'):
+                    label = curent_key[1]
+                    result = {}
+
+                    if region not in regions:
+                        regions.append(region)
+                        result["region"] = region
+
+                    match label:
+                        case "NEG":
+                            labels.append(label)
+                            result["NEG"] = item.value
+                        case "NEU":
+                            labels.append(label)
+                            result["NEU"] = item.value
+                        case "POS":
+                            labels.append(label)
+                            result["POS"] = item.value
+                        case _:
+                            continue
+
+                    if 'region' not in result.keys():
+                        for i in test:
+                            if(i['region'] == region):
+                                i[label] = item.value
+                                continue
+                    else:
+                        test.append(result)
+
+        return jsonify(output=test)
+
+# Function to return data on solar in all regoin of melbourne from couchdb views and aurin data
+
+
+class TweetsOnSolar(Resource):
+    def get(self):
+        print("Entering database")
+        # get the related aurin data
+        # aurin_result = get_aurin(search_by="topic", search_topic="solar")
+        test = []
+        regions = []
+        labels = []
+
+        # query couchdb view to get the related data
+        for db_name in DB_LIST:
+            region = ''
+            match db_name:
+                case "twitter_north":
+                    region = "north"
+                case "twitter_south":
+                    region = "south"
+                case "twitter_west":
+                    region = "west"
+                case "twitter_east":
+                    region = "east"
+                case _:
+                    continue
+
+            db = couch[db_name]
+            rows = db.view('SentimentInfo/SentimentOfKey',
+                           reduce=True, group=True, group_level=2)
+
+            for item in rows:
+                curent_key = item.key
+                if(curent_key[0] == 'solar'):
+                    label = curent_key[1]
+                    result = {}
+
+                    if region not in regions:
+                        regions.append(region)
+                        result["region"] = region
+
+                    match label:
+                        case "NEG":
+                            labels.append(label)
+                            result["NEG"] = item.value
+                        case "NEU":
+                            labels.append(label)
+                            result["NEU"] = item.value
+                        case "POS":
+                            labels.append(label)
+                            result["POS"] = item.value
+                        case _:
+                            continue
+
+                    if 'region' not in result.keys():
+                        for i in test:
+                            if(i['region'] == region):
+                                i[label] = item.value
+                                continue
+                    else:
+                        test.append(result)
+        return jsonify(output=test)
+# Function to return data on electric cars in all regoin of melbourne from couchdb views and aurin data
+
+
+class TweetsOnElectricCars(Resource):
+    def get(self):
+        print("Entering database")
+        # get the related aurin data
+        # aurin_result = get_aurin(search_by="topic", search_topic="electric_cars")
+        test = []
+        regions = []
+        labels = []
+
+        # query couchdb view to get the related data
+        for db_name in DB_LIST:
+            region = ''
+            match db_name:
+                case "twitter_north":
+                    region = "north"
+                case "twitter_south":
+                    region = "south"
+                case "twitter_west":
+                    region = "west"
+                case "twitter_east":
+                    region = "east"
+                case _:
+                    continue
+
+            db = couch[db_name]
+            rows = db.view('SentimentInfo/SentimentOfKey',
+                           reduce=True, group=True, group_level=2)
+
+            for item in rows:
+                curent_key = item.key
+                if(curent_key[0] == 'electric_cars'):
+                    label = curent_key[1]
+                    result = {}
+
+                    if region not in regions:
+                        regions.append(region)
+                        result["region"] = region
+
+                    match label:
+                        case "NEG":
+                            labels.append(label)
+                            result["NEG"] = item.value
+                        case "NEU":
+                            labels.append(label)
+                            result["NEU"] = item.value
+                        case "POS":
+                            labels.append(label)
+                            result["POS"] = item.value
+                        case _:
+                            continue
+
+                    if 'region' not in result.keys():
+                        for i in test:
+                            if(i['region'] == region):
+                                i[label] = item.value
+                                continue
+                    else:
+                        test.append(result)
+
+        return jsonify(output=test)
+
 
 api.add_resource(Tweet, '/api/tweet')
 api.add_resource(TweetOfNorth, '/api/tweet/north')
 api.add_resource(TweetOfSouth, '/api/tweet/south')
 api.add_resource(TweetOfWest, '/api/tweet/west')
 api.add_resource(TweetOfEast, '/api/tweet/east')
+api.add_resource(TweetsOnRecycling, '/api/tweet/recycling')
+api.add_resource(TweetsOnSolar, '/api/tweet/solar')
+api.add_resource(TweetsOnElectricCars, '/api/tweet/electric_cars')
 
 if __name__ == '__main__':
     couch = couchdb.Server(DB_URI)
-    app.run(port='8081',debug=True)
+    app.run(port='8081', debug=True)
