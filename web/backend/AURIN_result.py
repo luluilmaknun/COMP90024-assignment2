ca5082940f0e5951602f42228f9531590095117d
Lulu Ilmaknun Qurotaini <lulu.ilmaknun@ui.ac.id>
2022-05-11 15:56:29 +1000

    Update AURIN data analysis result

--- web/backend/AURIN_result.py
+++ web/backend/AURIN_result.py
@@ -1,71 +1,88 @@
 aurin_result = {
-  "description": {
-    "avg_dwelings": "Average number of dwelings",
-    "avg_dwelings_with_mv": "Average number of dwelings with at least one motor vehicle",
-    "avg_solar_installation": "Average number of solar installation",
-    "perha_solar_installation": "Ratio number of solar installation per hectare",
-    "ratio_dwelings_with_mv": "Ratio number of dwelings with at least one motor vehicle per total dwelings",
-    "total_dwelings": "Total number of dwelings",
-    "total_dwelings_with_mv": "Total number of dwelings with at least one motor vehicle",
-    "total_solar_installation": "Total number of solar installation"
-  },
-  "result": {
-    "east": {
-      "electric_cars": {
-        "avg_dwelings": 4145.208333333333,
-        "avg_dwelings_with_mv": 4028.5208333333335,
-        "ratio_dwelings_with_mv": 0.9718500276423582,
-        "total_dwelings": 198970,
-        "total_dwelings_with_mv": 193369
-      },
-      "solar": {
-        "avg_solar_installation": 762.875,
-        "perha_solar_installation": 0.12271168083301048,
-        "total_solar_installation": 36618
-      }
+    "description": {
+        "percentage_national_parks": "Ratio number of national parks per land area",
+        "percentage_protected_area": "Ratio number of protected area per land area",
+        "perha_solar_installation": "Ratio number of solar installation per hectare",
+        "perperson_mv": "Ratio number of motor vehicle per population",
+        "perperson_solar_installation": "Ratio number of solar installation per population",
+        "ratio_agriculture_per_population": "Ratio total of agricultures per population",
+        "ratio_dwelings_with_mv": "Ratio number of dwelings with at least one motor vehicle per total dwelings",
+        "total_agriculture_value": "Total of agricultures value",
+        "total_dwelings_with_mv": "Total number of dwelings with at least one motor vehicle",
+        "total_solar_installation": "Total number of solar installation"
     },
-    "north": {
-      "electric_cars": {
-        "avg_dwelings": 5336.547619047619,
-        "avg_dwelings_with_mv": 5168.702380952381,
-        "ratio_dwelings_with_mv": 0.9685479733196511,
-        "total_dwelings": 896540,
-        "total_dwelings_with_mv": 868342
-      },
-      "solar": {
-        "avg_solar_installation": 587.680981595092,
-        "perha_solar_installation": 0.3913794540495222,
-        "total_solar_installation": 95792
-      }
-    },
-    "south": {
-      "electric_cars": {
-        "avg_dwelings": 5773.511627906977,
-        "avg_dwelings_with_mv": 5570.558139534884,
-        "ratio_dwelings_with_mv": 0.9648474790643717,
-        "total_dwelings": 248261,
-        "total_dwelings_with_mv": 239534
-      },
-      "solar": {
-        "avg_solar_installation": 1319.0232558139535,
-        "perha_solar_installation": 0.254205838693716,
-        "total_solar_installation": 56718
-      }
-    },
-    "west": {
-      "electric_cars": {
-        "avg_dwelings": 4639.714285714285,
-        "avg_dwelings_with_mv": 4481.469387755102,
-        "ratio_dwelings_with_mv": 0.9658933959691396,
-        "total_dwelings": 227346,
-        "total_dwelings_with_mv": 219592
-      },
-      "solar": {
-        "avg_solar_installation": 1035.0204081632653,
-        "perha_solar_installation": 0.17033619197381883,
-        "total_solar_installation": 50716
-      }
+    "result": {
+        "east": {
+            "electric_cars": {
+                "perperson_mv": 0.3136450971516028,
+                "ratio_dwelings_with_mv": 0.9721816707218167,
+                "total_dwelings_with_mv": 203779
+            },
+            "recycling": {
+                "percentage_national_parks": 0.18104496597617015,
+                "percentage_protected_area": 0.20324318631941773,
+                "ratio_agriculture_per_population": 483.64658358115105,
+                "total_agriculture_value": 250920270.5
+            },
+            "solar": {
+                "perha_solar_installation": 0.1279971846484693,
+                "perperson_solar_installation": 0.05911541862212868,
+                "total_solar_installation": 38408
+            }
+        },
+        "north": {
+            "electric_cars": {
+                "perperson_mv": 0.31142777786423775,
+                "ratio_dwelings_with_mv": 0.9686029507417226,
+                "total_dwelings_with_mv": 863711
+            },
+            "recycling": {
+                "percentage_national_parks": 0.0561674135773169,
+                "percentage_protected_area": 0.06182625794005033,
+                "ratio_agriculture_per_population": 13.39140964616345,
+                "total_agriculture_value": 24381927.42
+            },
+            "solar": {
+                "perha_solar_installation": 0.4730072121445115,
+                "perperson_solar_installation": 0.034026936552907304,
+                "total_solar_installation": 94370
+            }
+        },
+        "south": {
+            "electric_cars": {
+                "perperson_mv": 0.28762177644566667,
+                "ratio_dwelings_with_mv": 0.9648474790643717,
+                "total_dwelings_with_mv": 239534
+            },
+            "recycling": {
+                "percentage_national_parks": 0.025739696950139477,
+                "percentage_protected_area": 0.048445130477809094,
+                "ratio_agriculture_per_population": 764.8462391855227,
+                "total_agriculture_value": 338451104.22999996
+            },
+            "solar": {
+                "perha_solar_installation": 0.254205838693716,
+                "perperson_solar_installation": 0.06810445246372257,
+                "total_solar_installation": 56718
+            }
+        },
+        "west": {
+            "electric_cars": {
+                "perperson_mv": 0.2642344738561814,
+                "ratio_dwelings_with_mv": 0.9657997324075761,
+                "total_dwelings_with_mv": 217275
+            },
+            "recycling": {
+                "percentage_national_parks": 0.02242240027076106,
+                "percentage_protected_area": 0.03490821677373444,
+                "ratio_agriculture_per_population": 153.40065672601244,
+                "total_agriculture_value": 94674692.35000001
+            },
+            "solar": {
+                "perha_solar_installation": 0.18063754683537164,
+                "perperson_solar_installation": 0.06075295075806879,
+                "total_solar_installation": 49956
+            }
+        }
     }
-  }
 }
-  
